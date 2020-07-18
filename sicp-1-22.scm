a2624e46a7ce629f17c7ba2c19a7cd445d889971
Andrey Serikov <grtrash2@gmail.com>
2020-07-18 19:01:03 +0300

    1-22 repl-it

--- sicp-1-22.scm
+++ sicp-1-22.scm
@@ -1,29 +1,96 @@
+; https://repl.it/@AndreySerikov/sicp-1-22#main.scm
+
+(define (date2runtime date)
+  ; HACK
+  ; wraps around occasionally!
+  (+  
+     (* (date-hour date) 60 60 1000) 
+     (* (date-minute date) 60 1000) 
+     (* (date-second date) 1000) 
+     (date-millisecond date)
+  )
+)
+(define (runtime) (date2runtime (current-date)))
+
 (define (smallest-divisor n)
   (find-divisor n 2))
+
 (define (find-divisor n d)
   (cond ((> (square d) n) n)
-        ((= (% n d) 0) d)
+        ((= (mod n d) 0) d)
         (else (find-divisor n (+ d 1)))))
+
 (define (square x)
   (* x x))
 
 (define (prime? n)
-  (= n (smallest-divisor n)))
+  (= (smallest-divisor n) n))
 
 (define (expmod b e m)
   (cond ((= e 0) 1)
-        ((even? e) (% (expmod (square b) (/ e 2) m) m))
-        (else (% (* b (expmod b (- e 1) m)) m))))
-(define (square x)
-  (* x x))
-(define (even? x)
-  (= (% x 2) 0))
+        ((even? e) (mod (square (expmod b (/ e 2)) m) m))
+        (else (mod (* b (expmod b (- e 1) m)) m))))
 
-;(define (test-prime n)
-  ;(print n)
-  ;(start-test n 0))
-;(define (start-test n t)
-;  1)
+(define (timed-prime-test n)
+  (start-prime-test n (runtime)))
 
-;(test-prime 17)
+(define (start-prime-test n start-time)
+  (if (prime? n) (report-prime n (- (runtime) start-time)) #f))
 
+(define (report-prime n elapsed-time)
+  (newline)
+  (display n)
+  (display " *** ")
+  (display elapsed-time)
+  #t)
+
+(define (search-for-primes from amount)
+  (if (even? from) (search-iter (+ from 1) amount) (search-iter from amount)))
+
+(define (search-iter n amount)
+  (if (= amount 0) #t (search-iter (+ n 2) (if (timed-prime-test n) (- amount 1) amount))))
+
+
+(search-for-primes 1e3 3)
+(search-for-primes 1e4 3)
+(search-for-primes 1e5 3)
+(search-for-primes 1e6 3)
+(search-for-primes 1e7 3)
+(search-for-primes 1e8 3)
+(search-for-primes 1e9 3)
+
+; 1009 *** 3
+; 1013 *** 3
+; 1019 *** 3
+; 10007 *** 8
+; 10009 *** 10
+; 10037 *** 11
+; 100003 *** 21
+; 100019 *** 20
+; 100043 *** 22
+; 1000003 *** 66
+; 1000033 *** 66
+; 1000037 *** 69
+; 10000019 *** 210
+; 10000079 *** 212
+; 10000103 *** 214
+; 100000007 *** 666
+; 100000037 *** 666
+; 100000039 *** 668
+; 1000000007 *** 2109
+; 1000000009 *** 2111
+; 1000000021 *** 2112
+
+; sqrt(10)    = 3.16227766
+; 2111 / 666  = 3,16
+; 666 / 212   = 3,14
+; 212 / 66    = 3,21
+; 66 / 21     = 3,14
+; 21 / 10     = 2,1
+; 10 / 3      = 3,33
+
+; (timed-prime-test 13)
+; (timed-prime-test 15)
+
+; (display (expmod 2 8 200))
+; (newline)
