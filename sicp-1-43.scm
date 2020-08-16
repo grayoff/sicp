a05fb2552c452e882bc1e29cd05793dbd0f05321
Andrey Serikov <grtrash2@gmail.com>
2020-08-16 08:51:08 +0300

    1-43+

--- sicp-1-43.scm
+++ sicp-1-43.scm
@@ -21,5 +21,19 @@
         ((even? n) (double (repeated f (/ n 2))))
         (else (compose f (repeated f (- n 1))))))
 
+(define (acc comb nul term a next b)
+  (if (> a b)
+      nul
+      (comb (term a)
+            (acc comb nul term (next a) next b))))
+
+(define (repeated2 f n)
+  (acc compose id (lambda (x) f) 1 inc n))
+       
+
 ((repeated square 2) 5)
 ((repeated inc 1000) 0)
+
+((repeated2 square 2) 5)
+((repeated2 inc 1000) 0)
+
